e07d2af8763780af29a8dcd2a5352ea5e51bf0fe
Shehan Atukorala <shehanatukorala@gmail.com>
2017-10-23 21:48:11 -0400

    Update script.js

--- script.js
+++ script.js
@@ -11,22 +11,22 @@ $(document).ready(function(){
         }
         var settings = [];
         $.extend(settings, defaults, options);
-        var numberOfPaginationLinks = Math.ceil((studentList / settings.itemsPerPage));
+        var numberOfPaginationLinks = Math.ceil((studentList / settings.itemsPerPage));//getting the appropriate number of pagination links to use
         $("<ul id='link_list'></ul>").prependTo(paginationContainer);
         for (var i = 1 ; i < numberOfPaginationLinks + 1; i++){
             $("<li><a id='"+i +"'>"+i +"</a></li>").appendTo($("#link_list"));
         }
         var counter = 0;
-        for (var i=0; i < studentList; i+= 10){
+        for (var i=0; i < studentList; i+= 10){//for loop that inserts the required amount of div tags for all students to fit with a max of 10 students per page
             $('<div id="page'+counter+'"></div>').insertBefore($(".cf")[i]);
             counter++;
         }
         var div = $("#page0");
         for (var i = 0 ; i < studentList; i++){
             if ( (i) % 10 == 0 ){
-                div = $("#page"+((i)/10));
+                div = $("#page"+((i)/10));//switchs to next div when the current div has 10 students
             }
-            $($(".cf")[i]).appendTo(div);
+            $($(".cf")[i]).appendTo(div);//moving students to the appropriate div 
         } 
     };
    
@@ -35,7 +35,7 @@ $(document).ready(function(){
         var numberOfPages = Math.ceil(studentList / itemsPerPage);
         
         const id = 0;
-        for(var i = 0; i <= numberOfPages; i++){
+        for(var i = 0; i <= numberOfPages; i++){//only shows page 1
         if (i != id){
             $("#page"+i).css("display","none");
         }
@@ -43,7 +43,7 @@ $(document).ready(function(){
             $("#page"+i).css("display","block");
         }}
     };
-    (function getStudentList(){
+    (function getStudentList(){//function that get the amount of students in the HTML page and invokes appendPageLinks function to start the application
         var itemsToPaginate = $(".cf").length;
         appendPageLinks(itemsToPaginate);
         startPage(itemsToPaginate);
@@ -56,7 +56,7 @@ $(document).ready(function(){
 
         }
         else{
-        search_list($("#search").val());
+        search_list($("#search").val());//make the search field empty after clicking the search button
         $("#search").val() == '';
         }
     });
@@ -67,7 +67,7 @@ $(document).ready(function(){
         var itemsToPaginate = $(".cf").length;
         var itemsPerPage = 10;
         var numberOfPages = Math.ceil(itemsToPaginate / itemsPerPage);
-        for (var i = 0 ; i < numberOfPages; i++){
+        for (var i = 0 ; i < numberOfPages; i++){//showing all pages 
             $("#page"+i).show();
         }
         var found = 'false';
@@ -83,23 +83,23 @@ $(document).ready(function(){
             }
             
         });
-        if (counter == studentList.length){
+        if (counter == studentList.length){//if invalid search value is entered refresh the page 
             alert('User Not Found');
             location.reload();
         }
         else{
-            $("#search").val('');
+            $("#search").val('');//if valid search value is entered call the showPage function with the matced array as the argument
             console.log(matched);
             showPage(0,matched)
         }
-        console.log(studentList)
+        
     }
 
-document.getElementById('pagination_tags').addEventListener('click', (event) => {
+document.getElementById('pagination_tags').addEventListener('click', (event) => {//event delegation
     showPage(event.target.id, []);//gets the id of the child element through event bubbling and invokes the showPage() function
 });    
 
-function showPage(id, studentList){
+function showPage(id, studentList){//function that controls what is shown on the page
     
     var itemsToPaginate = $(".cf").length;
     var itemsPerPage = 10;
@@ -108,11 +108,11 @@ function showPage(id, studentList){
         if (studentList.length != 0){
             var searchDiv = $("#page0")
             //console.log(studentList.length);
-            for (var i = 0 ; i < numberOfPages; i++){
+            for (var i = 0 ; i < numberOfPages; i++){//hiding existing students so as to not to interfer with the search results students
                 $("#page"+i).children().hide();
             }
             for (var i = 0 ; i < studentList.length ; i++){
-                if (i % 10 == 0 && i != 0){
+                if (i % 10 == 0 && i != 0){// if the search results exceeds 10 students add the remaining students to div page 2, div page 3 and so on...
                     searchDiv = $("#page"+ (i/10));
                     console.log(studentList[i]);
                 }
@@ -122,14 +122,14 @@ function showPage(id, studentList){
             }
             for (var i = 0 ; i < numberOfPages; i++){
                 if (i != 0){
-                    $("#page"+i).hide();
+                    $("#page"+i).hide();//show div page 1 after the students matching the search results have been sorted into the HTML
                 }
                 else{
                     $("#page"+i).show();
                 }
             }
         }
-         else{
+         else{//if a pagination link is click hide all pages except the page with the id that matchs the link
         for(var i = 0; i <= numberOfPages; i++){
         if (i != page){
             
